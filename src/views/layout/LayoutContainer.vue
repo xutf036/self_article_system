2a461b42405130385e83a43ee42ce245f7e4945a
xutf036 <2487868237@qq.com>
2024-05-12 16:58:41 +0800

    layout页面架子搭建

--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,7 +1,116 @@
+<script setup>
+import { Memo, Management, UserFilled, Edit, Avatar, EditPen, CaretBottom, SwitchButton } from '@element-plus/icons-vue'
+</script>
 <template>
-  <div>
-    布局架子
-    <!-- 二级路由出口 -->
-    <router-view></router-view>
-  </div>
+  <el-container class="layout-container">
+    <!-- 侧边栏 -->
+    <el-aside width="200px">
+      <div class="el-aside__logo"></div>
+      <el-menu active-text-color="#ffd04b" :default-active="$route.path" background-color="#232323" text-color="#fff" router>
+        <el-menu-item index="/article/channel">
+          <el-icon><Memo /></el-icon>
+          <span>文章分类</span>
+        </el-menu-item>
+        <el-menu-item index="/article/manage">
+          <el-icon><Management /></el-icon>
+          <span>文章管理</span>
+        </el-menu-item>
+        <el-sub-menu index="/user">
+          <template #title>
+            <el-icon><UserFilled /></el-icon>
+            <span>个人中心</span>
+          </template>
+          <el-menu-item index="user/profile">
+            <el-icon><Edit /></el-icon>
+            <span>基本资料</span>
+          </el-menu-item>
+          <el-menu-item index="user/avatar">
+            <el-icon><Avatar /></el-icon>
+            <span>更换头像</span>
+          </el-menu-item>
+          <el-menu-item index="user/password">
+            <el-icon><EditPen /></el-icon>
+            <span>重置密码</span>
+          </el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
+
+    <!-- 右侧主体部分 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>
+        <div>用户昵称：<strong>用户昵称</strong></div>
+        <el-dropdown placement="bottom-end">
+          <span class="el-dropdown__box">
+            <el-avatar :src="avatar"></el-avatar>
+            <el-icon><CaretBottom /></el-icon>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="a" :icon="Edit">基本资料</el-dropdown-item>
+              <el-dropdown-item command="b" :icon="Avatar">更换头像</el-dropdown-item>
+              <el-dropdown-item command="c" :icon="EditPen">重置密码</el-dropdown-item>
+              <el-dropdown-item command="d" :icon="SwitchButton">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </el-header>
+      <!-- 内容 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <!-- 底部 -->
+      <el-footer>文章管理系统 ©2024 Created by 风铭茗</el-footer>
+    </el-container>
+  </el-container>
 </template>
+
+<style lang="scss" scoped>
+.layout-container {
+  height: 100vh;
+
+  .el-aside {
+    background-color: #232323;
+
+    &__logo {
+      height: 120px;
+      background: url('@/assets/logo.png') no-repeat center / 120px auto;
+    }
+
+    .el-menu {
+      border-right: none;
+    }
+  }
+
+  .el-header {
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .el-dropdown__box {
+      display: flex;
+      align-items: center;
+
+      .el-icon {
+        color: #999;
+        margin-left: 10px;
+      }
+
+      &:active,
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  .el-footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    color: #666;
+  }
+}
+</style>
